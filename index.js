5de84cfd7b7a38275d94caf770fcfbdf46a4f286
DarVetra <darvetra13@gmail.com>
2023-05-01 13:10:19 +0300

    switch to module type

--- index.js
+++ index.js
@@ -1,104 +1,62 @@
-// 'use strict'
-require('dotenv').config();
-const { Telegraf } = require('telegraf');
-const { message } = require('telegraf/filters');
+import dotenv from 'dotenv';
+dotenv.config();
+
+import axios from 'axios';
+import { Telegraf } from 'telegraf';
+
+import {
+    COINGECKO_API_URL,
+    Buttons,
+    buttonMainScreenOptions,
+    ScreenDescription,
+    dataFights,
+    optionsFights,
+    urlFights
+} from './src/const.js';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// import { Telegraf } from 'telegraf';
-// import { message } from 'telegraf/filters';
-// import axios from 'axios';
-const axios = require('axios');
 
-
-// env
-
-// const bot = new Telegraf(BOT_TOKEN);
-
-// const buttonOptions = {
-//     reply_markup: JSON.stringify({
-//         inline_keyboard: [
-//             [{text: 'Дары Синдри', callback_data: 'tournament-gifts'}],
-//             [{text: 'Турнир Громовержца', callback_data: 'tournament-thunderer'}],
-//             [{text: 'Курс TON', callback_data: 'ton-rate'}]
-//         ]
-//     })
-// };
-
-// bot.start((ctx) => ctx.replyWithPhoto({ source: 'src/img/town-crier.png' }, { caption: 'This is a caption!' }, buttonOptions));
+// Главный экран
 bot.start((ctx) => ctx.replyWithPhoto(
     { source: 'src/img/town-crier.png' },
     {
-        caption: 'Привет, дружище! Что ты хочешь узнать?',
+        caption: ScreenDescription.MAIN,
         reply_markup: JSON.stringify({
-            inline_keyboard: [
-                [{text: '🏆 Турнир Громовержца', callback_data: 'tournament-thunderer'}],
-                [{text: 'Дары Синдри', callback_data: 'tournament-gifts'}],
-                [{text: 'Информация о персонаже', callback_data: 'info'}],
-                [{text: 'Курс TON', callback_data: 'ton-rate'}]
-            ]
+            inline_keyboard: buttonMainScreenOptions
         })
     }
 ));
 
+
+// Главный экран, по возвращению
 bot.action('back', (ctx) => {
-    ctx.editMessageCaption('Что ты хочешь узнать?', {
+    ctx.editMessageCaption(ScreenDescription.BACK, {
         reply_markup: {
-            inline_keyboard: [
-                [{text: '🏆 Турнир Громовержца', callback_data: 'tournament-thunderer'}],
-                [{text: 'Дары Синдри', callback_data: 'tournament-gifts'}],
-                [{text: 'Информация о персонаже', callback_data: 'info'}],
-                [{text: 'Курс TON', callback_data: 'ton-rate'}]
-            ]
+            inline_keyboard: buttonMainScreenOptions
         }
     });
 });
 
-// user_info
-const urlUser = 'https://api.rotgar.game/users';
-const dataUser = {
-    state: 'get_by_hide_id',
-    hide_id: '4s0ikmhn'
-};
-// axios.post(url, {
-//     state: 'get_by_hide_id',
-//     hide_id: '4s0ikmhn'
-// })
-//     .then(response => console.log(response.data))
-//     .catch(error => console.error(error));
 
-
-// tournament
-const urlFights = 'https://api.rotgar.game/reports';
-const dataFights = {
-    "state": "report_by_fights",
-    "rating_fights": 0,
-    "from": "01-05-2023",
-    "to": "08-05-2023"
-};
-const optionsFights = {
-    headers: {
-        'Content-Type': 'application/json',
-    }
-};
-
-
-
+// Экран "Турнир Громовержца"
 bot.action('tournament-thunderer', (ctx) => {
     ctx.editMessageCaption('Хочешь увидеть лидеров Турнира Громовержца ? Эта функция будет здесь позже', {
         reply_markup: {
             inline_keyboard: [
-                [{ text: 'Назад', callback_data: 'back' }]
+                [{ text: Buttons.BACK, callback_data: 'back' }]
             ]
         }
     });
 });
 
+
+// Экран "Турнир "Дары Синдри""
 bot.action('tournament-gifts', (ctx) => {
     ctx.editMessageCaption('Хочешь увидеть лидеров турнира "Дары Синдри" ? Эта функция будет здесь позже', {
         reply_markup: {
             inline_keyboard: [
-                [{ text: 'Назад', callback_data: 'back' }]
+                [{ text: Buttons.BACK, callback_data: 'back' }]
             ]
         }
     });
@@ -108,46 +66,8 @@ bot.action('tournament-gifts', (ctx) => {
         .catch(error => console.error(error));
 });
 
-// bot.action('info', async (ctx) => {
-bot.action('info', (ctx) => {
-    ctx.editMessageCaption(
-`
-Информация о персонаже:
-ID Персонажа: ${ctx.from.hide_id}
-Имя: ${ctx.from.first_name}
-Уровень: ${ctx.from.user_level}
-Боевой рейтинг: ${ctx.from.user_rating}
-Тикеты: ${ctx.from.user_tickets}
-Очки сброса: ${ctx.from.reset_points}
-`
 
-        // `
-        // Информация о персонаже:\n +
-        // ID Персонажа: ${ctx.from.hide_id}\n
-        // Уровень: ${ctx.from.user_level}\n
-        // Здоровье: ${ctx.from.user_hp}\n
-        // Урон: ${ctx.from.user_damage}\n
-        // Тикеты: ${ctx.from.user_tickets}\n
-        // Тикеты: ${ctx.from.reset_points}\n
-        // `
-        , {
-        reply_markup: {
-            inline_keyboard: [
-                [{ text: 'Назад', callback_data: 'back' }]
-            ]
-        }
-    });
-
-    axios.post(urlUser, dataUser)
-        .then(response => console.log(response.data))
-        .catch(error => console.error(error));
-});
-
-// "https://api.coingecko.com/api/v3/simple/price?ids="&B10&"&vs_currencies=USD"
-const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3';
-
-
-
+// Экран "Курс TON"
 bot.action('ton-rate', (ctx) => {
     ctx.editMessageCaption(`Хочешь узнать курс TON? ${ctx.from.first_name}? ${ctx.from.first_name}`, {
         reply_markup: {
@@ -168,36 +88,7 @@ bot.action('ton-rate', (ctx) => {
 });
 
 
-
-
-
-
-
-
-// bot.help((ctx) => ctx.reply('Send me a sticker'));
-// bot.action('button_pressed', (ctx) => {
-//     ctx.reply('Button pressed!');
-// });
-
-// Ответ на стикер
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'));
-
-// Диалог для знакомстав бота с игроками
-// bot.reply('Здарова, бандиты!)')
-bot.hears('Привет, дружище 😁', (ctx) => ctx.reply('Что-то ребята вы загрустили, решил разбавить ваши постные лица своей довольной "рожей") Вдарим рока в этой дыре! 🎸🤘'));
-bot.hears('А ты хорош 🤣', (ctx) => ctx.reply('Турнир у вас какой-то скучный. Как он там называется? "Дары Синдри"? Пусть так, а кто лидирует? Не знаете? а я вот знаю!'));
-bot.hears('О! Ну давай, расскажи нам))');
-// Бот выкидывает список лидеров даров Синдри
-// Ладно, фигня все это . я анонсирую свой турнир
-// условия
-// название из мифологии, называет его своим приятель.
-// Именем "и.о. Короля" объявляю Турнир Громовержцев открытым!
-
-// твист: Глашатай это Локи
-// Озвучка бота с помощью , ,бота озвучки  tsslr или типа того
-// при выдаче квеста
-
-// Логи
+// Логи поля ввода
 bot.on('message', (ctx) => {
     console.log(ctx.message);
 });
@@ -216,6 +107,7 @@ bot.on('message', (ctx) => {
 //     .catch(error => {
 //         console.error(error);
 //     });
+
 
 bot.launch();
 
--- src/const.js
+++ src/const.js
@@ -1,4 +1,56 @@
-module.exports = {
-    JOKEN: ''
+
+// Описание экранов
+export const ScreenDescription = {
+    MAIN: ' — Привет, дружище! Что ты хочешь узнать?',
+    BACK: ' — Что ты хочешь узнать на этот раз?',
 };
 
+
+// Кнопки
+export const Buttons = {
+    TOURNAMENT_THUNDERER: '🏆 Турнир Громовержца',
+    TOURNAMENT_GIFTS: 'Дары Синдри',
+    RATE_TON: 'Курс TON',
+    CHARACTER_INFO: 'Информация о персонаже',
+    BACK: 'Назад',
+};
+
+
+// Кнопки основного экрана
+export const buttonMainScreenOptions = [
+    [{text: Buttons.TOURNAMENT_THUNDERER, callback_data: 'tournament-thunderer'}],
+    [{text: Buttons.TOURNAMENT_GIFTS, callback_data: 'tournament-gifts'}],
+    [{text: Buttons.RATE_TON, callback_data: 'ton-rate'}]
+];
+
+
+// Персонаж
+export const urlUser = 'https://api.rotgar.game/users';
+
+export const dataUser = {
+    state: 'get_by_hide_id',
+    hide_id: '4s0ikmhn'
+};
+
+
+// Бои и победы
+export const urlFights = 'https://api.rotgar.game/reports';
+
+export const dataFights = {
+    "state": "report_by_fights",
+    "rating_fights": 0,
+    "from": "01-05-2023",
+    "to": "08-05-2023"
+};
+
+export const optionsFights = {
+    headers: {
+        'Content-Type': 'application/json',
+    }
+};
+
+
+// Коингеко и ТОН
+export const COINGECKO_API_URL = 'https://api.coingecko.com/api/v3';
+
+// https://api.coingecko.com/api/v3/simple/price?ids="&B10&"&vs_currencies=USD
